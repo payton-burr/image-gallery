e6403dec494f8a9f6b1fa6f0b8b28147ff261843
Payton Burr <36023709+payton-burr@users.noreply.github.com>
2020-07-06 18:21:00 -0600

    generated images from API

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,27 @@
 const auth = '563492ad6f9170000100000180543d2a83114fb3a74dc663d43f3d53';
+const gallery = document.querySelector('.gallery');
+const searchInput = document.querySelector('.search-input');
+const submitButton = document.querySelector('.submit-button');
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch('https://api.pexels.com/v1/curated?per_page&page=1', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      Authorization: auth,
+    },
+  });
+
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement('div');
+    galleryImg.classList.add('gallery-img');
+    galleryImg.innerHTML = `
+    <img src="${photo.src.large}"></img>
+    <p>${photo.photographer}</p> `;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
