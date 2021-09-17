a266c7222b0264041d058f0e80fd48b40a57370e
payton-burr <paytonburr@outlook.com>
2021-09-16 22:57:32 -0600

    typed photos in FetchObject

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,20 @@ export type FetchObject = {
   next_page: string;
   page: number;
   per_page: number;
-  photos: [];
+  photos: [
+    {
+      [k: string]: {
+        avg_color: string;
+        height: number;
+        id: number;
+        liked: boolean;
+        photographer: string;
+        photographer_id: number;
+        photographer_url: string;
+        original: string;
+        large: string;
+      };
+    }
+  ];
   total_results: number;
 };
